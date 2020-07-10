a5528966630d3e19e1d8a261c6a8ed245401934a
TomBozik <tombozik@gmail.com>
2020-07-10 15:43:57 +0200

    change to tokens

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
@@ -11,23 +13,27 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => ['required'],
-            'password' => ['required']
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
         ]);
-
-
-        if(Auth::attempt($request->only('email', 'password'))){
-            return response()->json(Auth::user(), 200);
+    
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
         }
-        
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.']
-        ]);
+    
+        return $user->createToken($request->device_name)->plainTextToken;
     }
 
 
     public function logout()
     {
-        Auth::logout();
+        $request->user()->tokens()->delete();
+
+        return response()->json('logout', 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', 'RegisterController@register');
 Route::post('/login', 'LoginController@login');
-Route::post('/logout', 'LoginController@logout');
 
 Route::middleware(['auth:sanctum'])->group(function(){
-    // Route::get('/user', 'Api\UserController@authUser');
+    Route::post('/logout', 'LoginController@logout');
 });
